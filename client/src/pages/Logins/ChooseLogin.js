6d7e2a92f0fbd8b9323db82c68d3059fb5f84aba
Sachintha Waduge <isurusachin123@gmail.com>
2024-10-16 15:54:29 +0530

    change logong choose

--- client/src/pages/Logins/ChooseLogin.js
+++ client/src/pages/Logins/ChooseLogin.js
@@ -17,7 +17,7 @@ const ChooseLogin = () => {
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-blue-100 to-blue-200 p-4">
       <div className="flex flex-col items-center mb-12">
         <img className="w-32 mb-4 animate-bounce" src="images/Hospital-logo-W.png" alt="Hospital Logo" />
-        <h1 className="text-3xl font-bold text-gray-800">Select Your Login Type</h1>
+        <h1 className="text-3xl font-bold text-gray-800">Helasuwa.lk</h1>
         <p className="text-gray-600 mt-2">Choose your role to proceed</p>
       </div>
 
