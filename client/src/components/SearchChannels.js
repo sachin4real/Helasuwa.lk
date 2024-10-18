3c95eb3db203f5b4a69f2d99556741460a76f0d0
DULAN IMALKA <134499168+XdulanX@users.noreply.github.com>
2024-10-18 20:38:12 +0530

    Revert "Merge pull request #28 from sachin4real/Yomith#2"
    This reverts commit c3b02da9987ae49f35da47aae8a9e2ae0add8a35, reversing
    changes made to eb3c90c0da834928795117f76e516073619ed9bd.

--- client/src/components/SearchChannels.js
+++ client/src/components/SearchChannels.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
+import PatientHeader from "./PatientHeader";
 import SideBar from "./SideBar";
 import AllChannels from "./AllChannels";
 import { useParams } from "react-router-dom";
--- client/src/pages/Payment/Myprescriptions.js
+++ client/src/pages/Payment/Myprescriptions.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import RowPrescriptionView from '../../components/Payment/RowPrescriptionView';
 import PrescriptionDetails from '../../components/Payment/PrescriptionDetails';
 import PatientSideBar from '../../components/PatientSideBar';
-import Patientheader from '../../components/Payment/Patientheader';
+import Patientheader from '../../components/Payment/PatientHeader.js';
 
 const MyPrescriptions = () => {
   const [prescriptions, setPrescriptions] = useState([]);
